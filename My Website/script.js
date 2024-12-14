7f7d6101d8c5f69fe4d87963cb731a4641680d99
Aarogya-Kafle <aarogya.kafle1649@gmail.com>
2024-12-15 00:27:58 +0545

    Add files via upload

---
+++ My Website/script.js
@@ -0,0 +1,13 @@
+function home()
+{
+    window.location.replace("index.html");//for opening in the same tab
+    // window.open() for opening in a new tab
+}
+function about()
+{
+    window.location.replace("about.html");
+}
+function merchandise()
+{
+    window.location.replace("merchandise.html");
+}
